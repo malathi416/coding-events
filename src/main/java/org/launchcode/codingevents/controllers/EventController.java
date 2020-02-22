ac4d83df0a1198f6b3311eb44dd7c88aaf66aff7
Sai <ammu.416@gmail.com>
2020-02-21 22:14:27 -0600

    finished the exercise before bonus

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,12 +2,10 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -15,31 +13,29 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static  List<String> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
-//        List<String> events = new ArrayList<>();
-//        events.add("Code With Pride");
-//        events.add("Strange Loop");
-//        events.add("Apple WWDC");
-//        events.add("SpringOne Platform");
-//        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Strange Loop","Meeting");
+        events.put("Code with Pride","Conference");
+        events.put("LC 101 Graduation","Party");
         model.addAttribute("events",events);
         return "events/index";
     }
 
     //lives at /events/create
-    @GetMapping("create")
-    public String renderCreateEventForm(){
-        return "events/create";
-    }
-
-    @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName){
-        events.add(eventName);
-        return "redirect:";
-    }
+//    @GetMapping("create")
+//    public String renderCreateEventForm(Model model){
+//        model.addAttribute("title", "Create Event");
+//        return "events/create";
+//    }
+//
+//    @PostMapping("create")
+//    public String processCreateEventForm(@RequestParam String eventName,@RequestParam String description){
+//        events.put(eventName,description);
+//        return "redirect:";
+//    }
 
 
 
